0bb4a246b236c3ffaf56a5b61981f3d2c9e56703
mirjavedjeetu001 <javedmirjeetu.official@gmail.com>
2026-09-15 13:31:43 +0600

    Fix cPanel repository auto deployment path

--- deploy.php
+++ deploy.php
@@ -9,7 +9,7 @@
  */
 
 $SECRET = 'lakeview_deploy_secret_2024';
-$REPO_DIR = '/home/lakeviex/lakeview';
+$REPO_DIR = '/home/lakeviex/public_html';
 $PROJECT_DIR = '/home/lakeviex/public_html';
 $LOG_FILE = '/home/lakeviex/deploy.log';
 
@@ -44,13 +44,8 @@ $commands = [
     "cd $REPO_DIR && git fetch --prune origin prod 2>&1",
     "cd $REPO_DIR && git checkout prod 2>&1",
     "cd $REPO_DIR && git pull --ff-only origin prod 2>&1",
-    "cd $REPO_DIR && /bin/rsync -av --exclude='public_html' --exclude='.git' --exclude='node_modules' --exclude='.env' --exclude='storage' --exclude='bootstrap/cache' --exclude='public/storage' . $PROJECT_DIR/ 2>&1",
-    "cd $REPO_DIR && /bin/mkdir -p $PROJECT_DIR/build $PROJECT_DIR/images 2>&1",
-    "cd $REPO_DIR && /bin/rsync -av --delete public/build/ $PROJECT_DIR/build/ 2>&1",
-    "cd $REPO_DIR && /bin/rsync -av public/images/ $PROJECT_DIR/images/ 2>&1",
-    "cd $REPO_DIR && /bin/cp -f public_html/index.php $PROJECT_DIR/index.php 2>&1",
-    "cd $REPO_DIR && /bin/cp -f public_html/.htaccess $PROJECT_DIR/.htaccess 2>&1",
-    "/bin/rm -f $PROJECT_DIR/setup.php 2>&1",
+    "cd $PROJECT_DIR && /bin/mkdir -p build images 2>&1",
+    "cd $PROJECT_DIR && /bin/rm -f setup.php 2>&1",
     "cd $PROJECT_DIR && php artisan migrate --force 2>&1",
     "cd $PROJECT_DIR && php artisan config:cache 2>&1",
     "cd $PROJECT_DIR && php artisan route:cache 2>&1",
@@ -59,9 +54,19 @@ $commands = [
 ];
 
 foreach ($commands as $cmd) {
-    $output = shell_exec($cmd . ' 2>&1');
+    $outputLines = [];
+    $exitCode = 0;
+    exec($cmd, $outputLines, $exitCode);
+    $output = implode("\n", $outputLines);
     log_msg("CMD: $cmd");
+    log_msg("EXIT: $exitCode");
     log_msg("OUT: $output");
+
+    if ($exitCode !== 0) {
+        log_msg('Deployment failed.');
+        http_response_code(500);
+        exit('Deployment failed');
+    }
 }
 
 log_msg('Deployment complete!');
